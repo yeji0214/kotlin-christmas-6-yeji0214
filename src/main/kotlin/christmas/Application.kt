087d14c965210abee144a5be5ccf1cc93e648873
yeji0214 <1225yj50@naver.com>
2023-11-09 22:02:22 +0900

    음식의 메뉴와 가격 저장

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,9 @@
 package christmas
 
 fun main() {
-    TODO("프로그램 구현")
+    val appetizer = mapOf("양송이수프" to 6000, "타파스" to 5500, "시저샐러드" to 8000)
+    val main = mapOf("티본스테이크" to 55000, "바비큐립" to 54000, "해산물파스타" to 35000, "크리스마스파스타" to 25000)
+    val dessert = mapOf("초코케이크" to 15000, "아이스크림" to 5000)
+    val drink = mapOf("제로콜라" to 3000, "레드와인" to 60000, "샴페인" to 25000)
+
 }
