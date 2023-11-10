ec8e0d86f19c5ad445d0f7bf85c795ced0f485b7
yeji0214 <1225yj50@naver.com>
2023-11-10 16:25:28 +0900

    할인 계산 (평일, 주말), 금액 출력 형식 수정

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,4 +1,6 @@
 import camp.nextstep.edu.missionutils.Console
+import java.text.NumberFormat
+import java.util.*
 
 class InvalidDateException(message: String) : Exception(message)
 class DuplicateMenuException(message: String) : Exception(message)
@@ -17,13 +19,13 @@ fun isValidDate(input: String): Boolean {
     }
 }
 
-fun main() {
-    val appetizer = mapOf("양송이수프" to 6000, "타파스" to 5500, "시저샐러드" to 8000)
-    val main = mapOf("티본스테이크" to 55000, "바비큐립" to 54000, "해산물파스타" to 35000, "크리스마스파스타" to 25000)
-    val dessert = mapOf("초코케이크" to 15000, "아이스크림" to 5000)
-    val drink = mapOf("제로콜라" to 3000, "레드와인" to 60000, "샴페인" to 25000)
+val appetizer = mapOf("양송이수프" to 6000, "타파스" to 5500, "시저샐러드" to 8000)
+val main = mapOf("티본스테이크" to 55000, "바비큐립" to 54000, "해산물파스타" to 35000, "크리스마스파스타" to 25000)
+val dessert = mapOf("초코케이크" to 15000, "아이스크림" to 5000)
+val drink = mapOf("제로콜라" to 3000, "레드와인" to 60000, "샴페인" to 25000)
 
-    val days = listOf("목", "금", "토", "일", "월", "화", "수")
+val days = listOf("목", "금", "토", "일", "월", "화", "수")
+fun main() {
 
     println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
             "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
@@ -89,15 +91,19 @@ fun main() {
 
     val currentDate = date.toInt()
     val totalAmount = calculateTotalAmount(orderedItems, appetizer, main, dessert, drink)
-    val discountedTotalAmount = applyDiscounts(currentDate, totalAmount)
+    val discountedTotalAmount = applyDiscounts(currentDate, totalAmount, days, orderedItems)
+
+    val numberFormat = NumberFormat.getNumberInstance(Locale("en")) // Use Locale for comma formatting
+    val formattedTotalAmount = numberFormat.format(totalAmount)
+    val formattedDiscountedTotalAmount = numberFormat.format(discountedTotalAmount)
 
     println("12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
     println("<주문 메뉴>")
     for ((menuName, quantity) in orderedItems) {
         println("${menuName} ${quantity}개")
     }
-    println("\n<할인 전 총주문 금액>\n${totalAmount}원")
-    println("\n<할인 후 총주문 금액>\n${discountedTotalAmount}원")
+    println("\n<할인 전 총주문 금액>\n${formattedTotalAmount}원")
+    println("\n<할인 후 총주문 금액>\n${formattedDiscountedTotalAmount}원")
 }
 
 fun calculateTotalAmount(orderedItems: Map<String, Int>, appetizer: Map<String, Int>, main: Map<String, Int>, dessert: Map<String, Int>, drink: Map<String, Int>): Int {
@@ -115,16 +121,30 @@ fun calculateTotalAmount(orderedItems: Map<String, Int>, appetizer: Map<String, 
     return totalAmount
 }
 
-fun applyDiscounts(currentDate: Int, totalAmount: Int): Int {
+fun applyDiscounts(currentDate: Int, totalAmount: Int, days: List<String>, orderedItems: Map<String, Int>): Int {
+    val isWeekend = (currentDate % 7 == days.indexOf("금") || currentDate % 7 == days.indexOf("토"))
+    var additionalDiscount = 0
+
+    // Check for weekend discount
+    if (isWeekend) {
+        val mainMenusCount = orderedItems.filter { main.containsKey(it.key) }.values.sum()
+        additionalDiscount = 2023 * mainMenusCount
+    } else {
+        val dessertMenusCount = orderedItems.filter { dessert.containsKey(it.key) }.values.sum()
+        additionalDiscount = 2023 * dessertMenusCount
+    }
+
     val christmasDayDiscount = if (currentDate in 1..25) {
         1000 + (currentDate - 1) * 100
     } else {
         0
     }
+
     val totalDiscount = if (totalAmount > 10000) {
-        christmasDayDiscount
+        christmasDayDiscount + additionalDiscount
     } else {
         0
     }
+
     return totalAmount - totalDiscount
 }
