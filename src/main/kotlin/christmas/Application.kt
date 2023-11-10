9371a3186e813579ce04d7890a07a938d4e23fac
yeji0214 <1225yj50@naver.com>
2023-11-10 13:29:47 +0900

    주문할 메뉴와 개수 입력받기

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -36,5 +36,10 @@ fun main() {
 
     } while (!validDate)
 
-    println("date: $date")
+    println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+
+    val inputMenu = Console.readLine()
+    val menuItems = inputMenu.split(",")
+
+
 }
